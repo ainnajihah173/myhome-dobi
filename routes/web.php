9855d1cbe06f5820db8fb2e7b56761c6845444f3
ftnfaiqah <atien.yusof87@gmail.com>
2024-12-21 15:36:25 +0800

    route updated

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,10 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\DeliveryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BillingPaymentController;
+use App\Http\Controllers\ScheduleController;
 use App\Http\Controllers\StaffController;
+use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\ChemicalOrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -65,6 +68,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/customer-payment-page/{order_id}', [BillingPaymentController::class, 'customerPaymentPage'])->name('billing.customer.payment.page');
     Route::post('/billing/customer/payment', [BillingPaymentController::class, 'payOrder'])->name('billing.customer.payment');
     Route::get('/billing/invoice/{orderId}', [BillingPaymentController::class, 'generateInvoice'])->name('billing.invoice');
+
+    /*Manage Inventory and Stock*/
+    Route::resource('inventory', InventoryController::class);
+    /*Button show all order */
+    Route::get('/inventory/purchase', [InventoryController::class, 'purchase'])->name('inventory.purchase');
+    /*Route::delete('/chemical-orders/{id}', [InventoryController::class, 'destroy'])->name('chemical-orders.destroy');*/
+    Route::get('inventory/{inventory}/buy', [InventoryController::class, 'buy'])->name('inventory.buychemical');
+    // Route to handle the chemical order form submission
+    Route::post('chemical-orders', [ChemicalOrderController::class, 'store'])->name('chemical-orders.store');
+    // Route for deleting chemical order
+    Route::delete('/chemical-orders/{chemicalOrder}', [ChemicalOrderController::class,'destroy'])->name('chemical-orders.destroy');
 
     /*Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
